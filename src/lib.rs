0ada7e5eb1154d99f58aa024cf64cd073ab611ae
Sven Strittmatter <ich@weltraumschaf.de>
2020-03-22 21:08:37 +0100

    Working example
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use seed::{*, prelude::*};
+
+
+struct Model {
+    pub val: i32,
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            val: 0,
+        }
+    }
+}
+
+#[derive(Clone)]
+enum Msg {
+    Increment,
+}
+
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Increment => model.val += 1,
+    }
+}
+
+
+fn view(model: &Model) -> impl View<Msg> {
+    vec![
+        h1!["This is Picard"],
+        h2!["An Example Project with WASM"],
+        button![
+            simple_ev(Ev::Click, Msg::Increment),
+            format!("Hello, World × {}", model.val)
+        ]
+    ]
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    App::builder(update, view)
+        .build_and_start();
+}
